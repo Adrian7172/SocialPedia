ba8365e2f6daf4e86151f7d3cbb7152ccccf5707
Adrian7172 <amnchauhan7172@gmail.com>
2023-02-11 16:30:58 +0530

    register setup

--- client/src/components/InputField.jsx
+++ client/src/components/InputField.jsx
@@ -8,7 +8,6 @@ const InputField = ({ label, ...props }) => {
   return (
     <TextField
       fullWidth
-      // margin="normal"
       label={label}
       {...field}
       {...props}
--- client/src/pages/registerPage/index.jsx
+++ client/src/pages/registerPage/index.jsx
@@ -1,7 +1,29 @@
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
 import React from "react";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const theme = useTheme();
+  const smallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <FlexBetween width={"100vw"} height={"100vh"} flexDirection={"column"}>
+      <Box
+        boxSizing={"border-box"}
+        border={smallScreen ? "0rem" : "0.15rem solid"}
+        py={2}
+        borderRadius={3}
+        borderColor={theme.palette.secondary.main}
+        sx={{
+          maxWidth: smallScreen ? "100%" : "52rem",
+          width: "100%",
+          height: smallScreen ? "100%" : "85%",
+          maxHeight: smallScreen ? "100%" : "56rem",
+          minHeight: "570px",
+          px: "clamp(1rem, 1.5rem, 1.8rem)",
+        }}
+      ></Box>
+    </FlexBetween>
+  );
 };
 
 export default RegisterPage;
