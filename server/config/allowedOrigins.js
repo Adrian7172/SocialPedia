1350db951118037b6ab3cb03dceb01b837c30c17
Adrian7172 <amnchauhan7172@gmail.com>
2023-03-19 16:12:03 +0530

    modified

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    "https://socialpedia-983r.onrender.com"
+    "https://socialpedia-983r.onrender.com",
+    "https://localhost:3000"
 ]
 
 module.exports = allowedOrigins
