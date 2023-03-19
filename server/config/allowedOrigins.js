d5ebfeaa490f9517911e73685893466eaf653675
Adrian7172 <amnchauhan7172@gmail.com>
2023-03-19 17:17:44 +0530

    add origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     "https://socialpedia-983r.onrender.com",
-    "https://social-pedia-euh528pmi-adrian7172.vercel.app/",
+    "https://social-pedia-euh528pmi-adrian7172.vercel.app",
     "https://localhost:3000"
 ]
 
