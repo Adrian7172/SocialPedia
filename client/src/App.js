0392a70c758f113a8f2141e770c093793018bef6
Adrian7172 <amnchauhan7172@gmail.com>
2023-03-19 15:06:47 +0530

    modified

--- client/src/App.js
+++ client/src/App.js
@@ -50,15 +50,15 @@ function App() {
 
   // protectedRoute
   const ProtectedRoute = ({ children }) => {
-    // if (!user && !(children.type.name === "LoginPage" || children.type.name === "RegisterPage")) {
-    //   return <Navigate to="/login" />
-    // }
-    if (user && (children.type.name === "LoginPage" || children.type.name === "RegisterPage")) {
-      return <Navigate to="/" />
+    if (!user && !(children.type.name === "LoginPage" || children.type.name === "RegisterPage")) {
+      return <Navigate to="/login" />
     }
+    // if (user && (children.type.name === "LoginPage" || children.type.name === "RegisterPage")) {
+    //   return <Navigate to="/" />
+    // }
     return children;
   }
-
+  
   const router = createBrowserRouter([
     {
       path: "/",
@@ -88,11 +88,11 @@ function App() {
     },
     {
       path: "/login",
-      element: <LoginPage />
+      element: <ProtectedRoute><LoginPage /></ProtectedRoute>
     },
     {
       path: "/register",
-      element: <RegisterPage />
+      element: <ProtectedRoute><RegisterPage /></ProtectedRoute>
     },
 
   ])
