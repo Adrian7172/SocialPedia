368f7fabb71e74cbee165dd2291cef7a4775487f
Adrian7172 <amnchauhan7172@gmail.com>
2023-03-19 16:32:13 +0530

    modify

--- client/src/App.js
+++ client/src/App.js
@@ -92,7 +92,6 @@ function App() {
       path: "/register",
       element: <RegisterPage />
     },
-
   ])
 
   return (
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const uploadPicture = require("./uploadPicture");
 const Images = require("../model/Images");
-const { User_profiles } = require("../model/user_profiles");
+const User_profiles = require("../model/user_profiles");
 
 
 /* REGISTER */
